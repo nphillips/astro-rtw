bff5288a3425027f2f6a5190d4df822912b975f2
nphillips <nick@nphillips.com>
2023-03-05 10:52:55 -0500

    initial commit for this template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  // Enable React to support React JSX components.
+  integrations: [react(), tailwind()]
 });
